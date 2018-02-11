0e725af6033f82c69d62dda84a8508817cb0acc5
lambag <lamba92sachin@gmail.com>
2018-02-11 16:21:52 +0530

    debug2

--- app.js
+++ app.js
@@ -172,8 +172,8 @@ console.log('API server listening on port: 3000 or ', process.env.PORT)
 
 app.post('/makerLab', function (req, res){
   // let city = req.body.result.parameters['geo-city']; // city is a required param
-  console.log("Intent Name -> ", req.body.result.metadata['intentName']);
-  let list_type = req.body.result.parameters['list']; // city is a required param
+  // console.log("Intent Name -> ", req.body.result.metadata['intentName']);
+  // let list_type = req.body.result.parameters['list']; // city is a required param
   // if(list_type == "products"){
   //     callProducts().then((output) => {
   //       // Return the results of the weather API to Dialogflow
@@ -185,61 +185,72 @@ app.post('/makerLab', function (req, res){
   //       res.send(JSON.stringify({ 'speech': error, 'displayText': error }));
   //     });
   // }else{
+  console.log("DSf1");
     callUsers().then((output) => {
+      console.log("DSf4");
       // Return the results of the weather API to Dialogflow
       res.setHeader('Content-Type', 'application/json');
       res.send(JSON.stringify({ 'speech': output, 'displayText': output }));
     }).catch((error) => {
       // If there is an error let the user know
+      console.log("DSf5");
       res.setHeader('Content-Type', 'application/json');
       res.send(JSON.stringify({ 'speech': error, 'displayText': error }));
     });
   // }
 })
 
-function callProducts () {
-  return new Promise((resolve, reject) => {
-    let path = '/list_products';
-    http.get({host: serverHost, path: path}, (res) => {
-      let body = ''; // var to store the response chunks
-      res.on('data', (d) => { body += d; }); // store each response chunk
-      res.on('end', () => {
-        // After all the data has been received parse the JSON for desired data
-        let response = JSON.parse(body);
-        // Create response
-        let a = JSON.stringify(response)
-        let output = `Product list in MongoDB mLab ${a}`;
-        // response.forEach(function (product, i) {
-        //   output += i + " " product.name + " \n"
-        // })
-        // Resolve the promise with the output text
-        console.log(output);
-        resolve(output);
-      });
-      res.on('error', (error) => {
-        reject(error);
-      });
-    });
-  });
-}
+// function callProducts () {
+//   return new Promise((resolve, reject) => {
+//     let path = '/list_products';
+//     http.get({host: serverHost, path: path}, (res) => {
+//       let body = ''; // var to store the response chunks
+//       res.on('data', (d) => { body += d; }); // store each response chunk
+//       res.on('end', () => {
+//         // After all the data has been received parse the JSON for desired data
+//         let response = JSON.parse(body);
+//         // Create response
+//         let a = JSON.stringify(response)
+//         let output = `Product list in MongoDB mLab ${a}`;
+//         // response.forEach(function (product, i) {
+//         //   output += i + " " product.name + " \n"
+//         // })
+//         // Resolve the promise with the output text
+//         console.log(output);
+//         resolve(output);
+//       });
+//       res.on('error', (error) => {
+//         reject(error);
+//       });
+//     });
+//   });
+// }
 function callUsers () {
+  console.log("DSfdsz2");
+  var bodyString=JSON.stringify({"a": "b"})
+  var _headers = {
+    'Content-Length': Buffer.byteLength(bodyString)
+  };
   return new Promise((resolve, reject) => {
-    let path = '/create_user1';
-    http.get({host: serverHost, path: path}, (res) => {
-      let body = ''; // var to store the response chunks
-      res.on('data', (d) => { body += d; }); // store each response chunk
-      res.on('end', () => {
-        // After all the data has been received parse the JSON for desired data
-        let response = JSON.parse(body);
-        // Create response
-        let a = JSON.stringify(response)
-        let output = `Users list in MongoDB mLab ${a}`;
-        console.log(output);
-        resolve(output);
+    console.log("DSzsdf3", bodyString, _headers);
+    let path = '/create_user';
+    let req =   http.request({host: "maker-lab-lamba.herokuapp.com", path: path, method: "POST", header: _headers}, (res) => {
+        let body = ''; // var to store the response chunks
+        res.on('data', (d) => { body += d; }); // store each response chunk
+        res.on('end', () => {
+          // After all the data has been received parse the JSON for desired data
+          let response = JSON.parse(body);
+          // Create response
+          let a = JSON.stringify(response)
+          let output = `Users list in MongoDB mLab ${a}`;
+          console.log(output);
+          resolve(output);
+        });
+        res.on('error', (error) => {
+          reject(error);
+        });
       });
-      res.on('error', (error) => {
-        reject(error);
-      });
-    });
+      req.write(bodyString);
+      req.end();
   });
 }
